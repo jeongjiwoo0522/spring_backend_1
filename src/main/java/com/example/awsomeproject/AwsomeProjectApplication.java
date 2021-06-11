b51c170ccd2b7bb088f1484a27fe18876c8031f8
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-11 15:00:33 +0900

    init commit

---
+++ src/main/java/com/example/awsomeproject/AwsomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsomeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsomeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsomeProjectApplication.class, args);
+    }
+
+}
